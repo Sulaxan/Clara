3ddb48e29ca641f1ee41664e338504c51992d1fb
Encast <encastcore@gmail.com>
2019-11-24 21:03:17 -0500

    Add more features to spell manager

--- src/main/java/me/encast/clara/spell/SpellManager.java
+++ src/main/java/me/encast/clara/spell/SpellManager.java
@@ -1,17 +1,30 @@
 package me.encast.clara.spell;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import lombok.Getter;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class SpellManager {
 
+    private Plugin plugin;
     // Singleton instances of every spell
     private Map<String, Spell> registeredSpells = Maps.newConcurrentMap();
+    private Map<UUID, List<SpellCooldown>> cooldowns = Maps.newConcurrentMap();
 
-    public SpellManager() {
+    // Invalidating any players without any cooldowns
+    private final Consumer<UUID> GLOBAL_CALLBACK = uuid -> {
+        List<SpellCooldown> cooldowns = this.cooldowns.getOrDefault(uuid, null);
+        if(cooldowns != null && cooldowns.isEmpty())
+            this.cooldowns.remove(uuid);
+    };
+
+    public SpellManager(Plugin plugin) {
+        this.plugin = plugin;
     }
 
     public void registerSpell(Spell spell) {
@@ -28,5 +41,57 @@ public class SpellManager {
 
     public Spell getSpell(String id) {
         return registeredSpells.getOrDefault(id, null);
+    }
+
+    public SpellCooldown getCooldown(UUID uuid, Spell spell) {
+        List<SpellCooldown> cooldowns = this.cooldowns.getOrDefault(uuid, null);
+        if(cooldowns != null) {
+            for(SpellCooldown cooldown : cooldowns) {
+                if(cooldown.getSpellId().equals(spell.getId())) {
+                    return cooldown;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void addCooldown(UUID uuid, Spell spell) {
+        List<SpellCooldown> cooldowns = this.cooldowns.getOrDefault(uuid, Lists.newArrayList());
+        if(cooldowns.isEmpty())
+            this.cooldowns.put(uuid, cooldowns);
+        SpellCooldown cooldown = new SpellCooldown(
+                uuid,
+                spell.getId(),
+                System.currentTimeMillis(),
+                cooldowns,
+                GLOBAL_CALLBACK
+
+        );
+        cooldowns.add(cooldown);
+        cooldown.runTaskLater(plugin, spell.getCooldown() / 50);
+    }
+
+    @Getter
+    public class SpellCooldown extends BukkitRunnable {
+
+        private UUID uuid;
+        private String spellId;
+        private long startTime;
+        private List<SpellCooldown> cooldowns;
+        private Consumer<UUID> callback;
+
+        public SpellCooldown(UUID uuid, String spellId, long startTime, List<SpellCooldown> cooldowns, Consumer<UUID> callback) {
+            this.uuid = uuid;
+            this.spellId = spellId;
+            this.startTime = startTime;
+            this.cooldowns = cooldowns;
+            this.callback = callback;
+        }
+
+        @Override
+        public void run() {
+            cooldowns.remove(this);
+        }
     }
 }
