c70f4b7ff7b776d50f2097ed9714c05cc5f080d9
Encast <encastcore@gmail.com>
2019-12-03 21:33:00 -0500

    Update world generator, again

--- src/main/java/me/encast/clara/world/ClaraGenerator.java
+++ src/main/java/me/encast/clara/world/ClaraGenerator.java
@@ -2,7 +2,6 @@ package me.encast.clara.world;
 
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 
@@ -13,25 +12,55 @@ public class ClaraGenerator extends ChunkGenerator {
     @Override
     public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
         ChunkData chunk = createChunkData(world);
-        SimplexOctaveGenerator gen = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+        SimplexOctaveGenerator gen = new SimplexOctaveGenerator(new Random(world.getSeed()), 1);
         gen.setScale(0.005d);
+        if((x < -16 || x > 16) || (z < -16 || z > 16)) {
+            for(int i = 0; i < 16; i++) {
+                for(int k = 0; k < 16; k++) {
+                    int nx = x * 16 + i;
+                    int nz = z * 16 + k;
+                    double noise =  1.00 * gen.noise(nx, nz, 16, 0.25, true) +
+                                    0.28 * gen.noise(4 * nx, 4 * nz, 12, 0.2, true);
+                    noise = (noise + 1) / 2; // further normalizing between [0, infinity]
+                    noise = Math.abs(noise);
+                    int y = (int) (noise * 100d + 20d);
+                }
+            }
+        }
         for(int i = 0; i < 16; i++) {
             for(int k = 0; k < 16; k++) {
                 int nx = x * 16 + i;
                 int nz = z * 16 + k;
-                double elev = (1.00 * noise1(gen, nx,  nz)
-                        + 0.27 * noise1(gen, 2 * nx,  2 * nz)
-                        + 0.17 * noise1(gen, 4 * nx,  4 * nz)
-                        + 0.12 * noise1(gen, 8 * nx,  8 * nz)
-                        + 0.06 * noise1(gen, 16 * nx, 16 * nz)
-                        + 0.03 * noise1(gen, 32 * nx, 32 * nz));
-                elev /= 1.00 + 0.27 + 0.17 + 0.12 + 0.06 + 0.03;
-                elev = Math.pow(elev, 5.52);
-                int height = (int) (elev * 15 + 50d);
-                for(int j = 0; j < height; j++) {
+                double noise =  1.00 * gen.noise(nx, nz, 16, 0.72, true) +
+//                                0.52 * gen.noise(4 * nx, 4 * nz, 1, 1, true) +
+                                0.28 * gen.noise(4 * nx, 4 * nz, 12, 0.2, true) +
+                                0.07 * gen.noise(16 * nx, 16 * nz, 12, 0.1);
+//                                0.03 * gen.noise(32 * nx, 32 * nz, 1, 1, true);
+                noise = (noise + 1) / 2; // further normalizing between [0, infinity]
+                noise = Math.abs(noise);
+                int y = (int) (noise * 100d + 20d);
+
+
+                for(int j = 1; j < y; j++) {
                     chunk.setBlock(i, j, k, Material.STONE);
                 }
-                chunk.setBlock(i, height, k, Material.STONE);
+                chunk.setBlock(i, 0, k, Material.BEDROCK);
+                if(y <= world.getSeaLevel()) {
+                    for(int j = y; j <= world.getSeaLevel(); j++) {
+                        chunk.setBlock(i, j, k, Material.WATER);
+                    }
+                    chunk.setBlock(i, y, k, Material.SAND);
+                } else if(y <= world.getSeaLevel() + 5) {
+                    chunk.setBlock(i, y, k, Material.SAND);
+                } else if(y >= 176) {
+                    if(y >= 200) {
+                        chunk.setBlock(i, y, k, Material.SNOW);
+                    } else {
+                        chunk.setBlock(i, y, k, Material.STONE);
+                    }
+                } else {
+                    chunk.setBlock(i, y, k, Material.GRASS);
+                }
             }
         }
         return chunk;
@@ -41,33 +70,7 @@ public class ClaraGenerator extends ChunkGenerator {
         return gen.noise(nx, nz, 1, 1) / 2 + 0.25;
     }
 
-    private Material getMaterial(World world, int x, int z, double e) {
-        if (e < 0.035) {
-            world.setBiome(x, z, Biome.OCEAN);
-            return Material.WATER;
-        }
-        else if (e < 0.075) {
-            world.setBiome(x, z, Biome.BEACH);
-            return Material.SAND;
-        }
-        else if (e < 0.1) {
-            world.setBiome(x, z, Biome.FOREST);
-            return Material.GRASS;
-        }
-        else if (e < 0.5) {
-            world.setBiome(x, z, Biome.JUNGLE);
-            return Material.GRASS;
-        }
-        else if (e < 0.7) {
-            world.setBiome(x, z, Biome.SAVANNA);
-            return Material.GRASS;
-        }
-        else if (e < 1.0) {
-            world.setBiome(x, z, Biome.DESERT);
-            return Material.SAND;
-        }
-
-        world.setBiome(x, z, Biome.TAIGA);
-        return Material.SNOW_BLOCK;
+    private Material getMaterial(World world, double noise) {
+        return Material.GRASS;
     }
 }
