0209967c8ee89c181575fdc771d284d1b9a7130c
Encast <encastcore@gmail.com>
2019-12-10 19:36:28 -0500

    Add better looking islands per chunk

--- src/main/java/me/encast/clara/world/ClaraGenerator.java
+++ src/main/java/me/encast/clara/world/ClaraGenerator.java
@@ -3,6 +3,7 @@ package me.encast.clara.world;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.material.MaterialData;
 
 import java.util.Random;
 
@@ -15,11 +16,92 @@ public class ClaraGenerator extends ChunkGenerator {
     private static final int WORLD_SIZE = 10000; // WORLD_SIZE * WORLD_SIZE chunks
     private static final int CHUNKS = WORLD_SIZE / 16;
 
-    private static final int CUBE_Y_START = 95;
-    private static final int CUBE_Y_END = 100;
+    private static final int[][] CIRCLE = new int[16][16];
+    private static MaterialData[] ISLAND_MATERIALS;
+    private static final int Y_START = 91;
+    private static final int Y_END = 100;
 
     private static final byte ALIVE = 0b1;
     private static final byte DEAD = 0b0;
+
+    static {
+        ISLAND_MATERIALS = new MaterialData[] {
+               new MaterialData(Material.STONE),
+               new MaterialData(Material.STONE, (byte) 5),
+               new MaterialData(Material.STONE, (byte) 6)
+        };
+        CIRCLE[6][2] = 1;
+        CIRCLE[7][2] = 1;
+        CIRCLE[8][2] = 1;
+        CIRCLE[9][2] = 1;
+        CIRCLE[10][3] = 1;
+        CIRCLE[11][3] = 1;
+        CIRCLE[12][4] = 1;
+        CIRCLE[12][5] = 1;
+        CIRCLE[13][6] = 1;
+        CIRCLE[13][7] = 1;
+        CIRCLE[13][8] = 1;
+        CIRCLE[13][9] = 1;
+        CIRCLE[12][10] = 1;
+        CIRCLE[12][11] = 1;
+        CIRCLE[11][12] = 1;
+        CIRCLE[10][12] = 1;
+        CIRCLE[9][13] = 1;
+        CIRCLE[8][13] = 1;
+        CIRCLE[7][13] = 1;
+        CIRCLE[6][13] = 1;
+        CIRCLE[5][12] = 1;
+        CIRCLE[4][12] = 1;
+        CIRCLE[3][11] = 1;
+        CIRCLE[3][10] = 1;
+        CIRCLE[2][9] = 1;
+        CIRCLE[2][8] = 1;
+        CIRCLE[2][7] = 1;
+        CIRCLE[2][6] = 1;
+        CIRCLE[3][5] = 1;
+        CIRCLE[3][4] = 1;
+        CIRCLE[4][3] = 1;
+        CIRCLE[5][3] = 1;
+
+
+        // Overhang
+        CIRCLE[6][1] = 2;
+        CIRCLE[7][1] = 2;
+        CIRCLE[8][1] = 2;
+        CIRCLE[9][1] = 2;
+        CIRCLE[10][2] = 2;
+        CIRCLE[11][2] = 2;
+        CIRCLE[12][3] = 2;
+        CIRCLE[13][4] = 2;
+        CIRCLE[13][5] = 2;
+        CIRCLE[14][6] = 2;
+        CIRCLE[14][7] = 2;
+        CIRCLE[14][8] = 2;
+        CIRCLE[14][9] = 2;
+        CIRCLE[13][10] = 2;
+        CIRCLE[13][11] = 2;
+        CIRCLE[12][12] = 2;
+        CIRCLE[11][13] = 2;
+        CIRCLE[10][13] = 2;
+        CIRCLE[9][14] = 2;
+        CIRCLE[7][14] = 2;
+        CIRCLE[8][14] = 2;
+        CIRCLE[6][14] = 2;
+        CIRCLE[5][13] = 2;
+        CIRCLE[4][13] = 2;
+        CIRCLE[3][12] = 2;
+        CIRCLE[2][11] = 2;
+        CIRCLE[2][10] = 2;
+        CIRCLE[1][9] = 2;
+        CIRCLE[1][8] = 2;
+        CIRCLE[1][7] = 2;
+        CIRCLE[1][6] = 2;
+        CIRCLE[2][5] = 2;
+        CIRCLE[2][4] = 2;
+        CIRCLE[3][3] = 2;
+        CIRCLE[4][2] = 2;
+        CIRCLE[5][2] = 2;
+    }
 
     private byte[][] map;
 
@@ -30,15 +112,7 @@ public class ClaraGenerator extends ChunkGenerator {
         }
         ChunkData chunk = createChunkData(world);
         if(Math.abs(x) < CHUNKS && Math.abs(z) < CHUNKS) {
-            if(map[x + WORLD_SIZE / 2][z + WORLD_SIZE / 2] == ALIVE) {
-                for(int i = 0; i < 16; i++) {
-                    for(int k = 0; k < 16; k++) {
-                        for(int y = CUBE_Y_START; y <= CUBE_Y_END; y++) {
-                            chunk.setBlock(i, y, k, Material.GRASS);
-                        }
-                    }
-                }
-            }
+            generateIsland(chunk, random, biome);
         }
         return chunk;
     }
@@ -47,10 +121,37 @@ public class ClaraGenerator extends ChunkGenerator {
         return Material.GRASS;
     }
 
+    public void generateIsland(ChunkData data, Random random, BiomeGrid biome) {
+        for(int x = 0; x < 16; x++) {
+            for(int z = 0; z < 16; z++) {
+                int val = CIRCLE[x][z];
+                if(val == 1) {
+                    for(int y = Y_START; y <= Y_END; y++) {
+                        if(y != Y_END) {
+                            data.setBlock(x, y, z, ISLAND_MATERIALS[random.nextInt(ISLAND_MATERIALS.length)]);
+                        } else {
+                            // Change depending on the biome
+                            data.setBlock(x, y, z, Material.GRASS);
+                        }
+                    }
+                } else {
+                    for(int y = Y_END - 2 - random.nextInt(5); y <= Y_END - 1; y++) {
+                        if(y != (Y_END - 1)) {
+                            data.setBlock(x, y, z, ISLAND_MATERIALS[random.nextInt(ISLAND_MATERIALS.length)]);
+                        } else {
+                            // Change depending on the biome
+                            data.setBlock(x, y, z, Material.GRASS);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private byte[][] getMap(int worldSize, Random random) {
         byte[][] world = new byte[worldSize][worldSize];
         for(int i = 0; i < world.length; i++) {
-            for(int j = 0; j < world[i].length; i++) {
+            for(int j = 0; j < world[i].length; j++) {
                 world[i][j] = random.nextDouble() < CHANCE_TO_LIVE ? ALIVE : DEAD;
             }
         }
